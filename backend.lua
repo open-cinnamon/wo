e1fa9a964f8057ece336b9f66e0d1c2d677aacd4
Wys <wys-workshop@outlook.fr>
2024-12-27 18:51:58 +0100

    idk...

--- backend.lua
+++ backend.lua
@@ -2,6 +2,9 @@ local system = require('system')
 local backend = {} -- For the front end ... 
 backend.__index = backend
 
-
+--[[
+  Make a file (and update this file every time we launch the application)
+  When we download an application, we look at the server, and dependencies's servers
+]]
 
 return backend -- RTS medieval
